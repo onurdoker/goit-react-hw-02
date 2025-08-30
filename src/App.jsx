11a2edfcaffdc415ffdbdd05b173d4fdca94b78e
Onur DÖKER <odoker@msn.com>
2025-08-30 23:58:07 +0300

    Implement local storage for feedback counts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import "./App.css";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Description from "./description/Description.jsx";
 import Option from "./option/Option.jsx";
 import Feedback from "./feedback/Feedback.jsx";
 
 function App() {
-  const [good, setGood] = useState (0);
-  const [neutral, setNeutral] = useState (0);
-  const [bad, setBad] = useState (0);
+  const [good, setGood] = useState (() => {
+    return parseInt (localStorage.getItem ("good")) || 0;
+  });
+  const [neutral, setNeutral] = useState (() => {
+    return parseInt (localStorage.getItem ("neutral")) || 0;
+  });
+  const [bad, setBad] = useState (() => {
+    return parseInt (localStorage.getItem ("bad")) || 0;
+  });
+
+  useEffect (() => {
+        localStorage.setItem ("good",
+            good);
+        localStorage.setItem ("neutral",
+            neutral);
+        localStorage.setItem ("bad",
+            bad);
+      },
+      [
+        good,
+        neutral,
+        bad]);
 
   const handleCount = (choice) => {
     if (choice === "Good") {
