1f7121c14a1598db0b6b9734b2b3264211c09048
Onur DÖKER <odoker@msn.com>
2025-08-30 21:40:19 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+
+  return (
+      <div>
+        <h1>
+          Sip Happens Café
+        </h1>
+        <p>
+          Please leave your feedback about our services by selecting one of the options below.
+        </p>
+      </div>
+  );
+}
+
+export default App;
