bf1fd9bbbee0dffae62841ebb196b633dd808dba
Onur DÖKER <odoker@msn.com>
2025-08-30 23:29:57 +0300

    Add Feedback component to display rating results

---
+++ src/feedback/Feedback.jsx
@@ -0,0 +1,39 @@
+import css from "./Feedback.module.css";
+
+const Feedback = ({
+  good,
+  bad,
+  neutral,
+}) => {
+
+  const totalFeedback = good + bad + neutral;
+
+  const positiveFeedback = Math.round ((good / totalFeedback) * 100);
+
+  if (totalFeedback === 0) {
+    return (
+
+        <div className={css.feedback}>
+          No feedback yet
+        </div>
+    );
+  } else {
+    return (
+
+        <div className={css.feedback}>
+          <div>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+          </div>
+          <div>
+            <p>Total: {totalFeedback}</p>
+            <p>Positive: {positiveFeedback}</p>
+          </div>
+        </div>
+    );
+  }
+
+};
+
+export default Feedback;
